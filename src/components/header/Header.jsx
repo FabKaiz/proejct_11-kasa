d2a9ccdd822c2424b840a1969100b18c0ec23c97
FabKaiz <cacrfabfr@gmail.com>
2023-09-07 11:20:07 +0200

    [+] - Config SCSS + Router + assets

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,35 @@
 import './header.scss'
+import Logo from '../../../public/assets/LOGO.png';
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
+    <header className="header">
+      <div className="header__logo">
+        <img src={Logo} alt="Brand logo" />
+      </div>
+
+      <nav className="header__nav">
+        <ul>
+          <li>
+            <NavLink
+              to={'/'}
+              className="header__nav-link"
+            >
+              Accueil
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={'/about'}
+              className="header__nav-link"
+            >
+              A propos
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {
-  createBrowserRouter,
+  createBrowserRouter, createRoutesFromElements, Route,
   RouterProvider,
 } from "react-router-dom";
 
@@ -11,22 +11,18 @@ import './styles/main.scss'
 import Error from "./pages/error/Error.jsx";
 import About from "./routes/About.jsx";
 import Logements from "./routes/Logements.jsx";
+import Layout from "./pages/Layout.jsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <Error />,
-  },
-  {
-    path: "about",
-    element: <About />,
-  },
-  {
-    path: "logements/:logementsId",
-    element: <Logements />,
-  },
-]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<Layout />}>
+      <Route path="/" element={<Root />} />
+      <Route path="about" element={<About />} />
+      <Route path="logements/:logementsId" element={<Logements />} />
+      <Route path="*" element={<Error />} />
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
