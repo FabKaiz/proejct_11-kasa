abef14320b5e6a561115b83df2308f98e84f6077
FabKaiz <cacrfabfr@gmail.com>
2023-09-15 09:51:08 +0200

    [+] - Started accordion

--- src/pages/logement/LogementPage.jsx
+++ src/pages/logement/LogementPage.jsx
@@ -1,6 +1,7 @@
 import BannerSwiper from '../../components/banner-swiper/BannerSwiper.jsx'
 import './logementPage.scss'
 import LogementHeader from '../../components/logement-header/LogementHeader.jsx'
+import Accordion from "../../components/accordion/Accordion.jsx";
 
 const LogementPage = ({ logement }) => {
   console.log(logement)
@@ -10,6 +11,8 @@ const LogementPage = ({ logement }) => {
       <BannerSwiper images={pictures} />
       <section className='logement__page-content'>
         <LogementHeader title={title} location={location} host={host} tags={tags} rating={rating} />
+        <Accordion title='Description' content={description} />
+        <Accordion title='Description' content={description} />
       </section>
     </div>
   )
